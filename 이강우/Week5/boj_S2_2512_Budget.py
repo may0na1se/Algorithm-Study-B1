60f8d234a64f3896201e35a459d70d5a78377aa3
kangwoo0609 <leekangwoo7@gmail.com>
2025-10-23 20:47:47 +0900

    [boj] 예산 / S2 / 오래걸림

--- 이강우/Week5/boj_S2_2512_Budget.py
+++ 이강우/Week5/boj_S2_2512_Budget.py
@@ -1,36 +1,41 @@
 '''
 문제: https://www.acmicpc.net/problem/2512
-접근: 총 예산을 배정해야하는 개수만큼 나눠서 limit를 정한다.
-평균 예산보다 큰 애들을 세주고, 평균 예산보다 작은 애들을 평균 예산에서 빼서 남는 예산을 구한다.
-그리고 만약 총 요구가 예산보다 더 크다면, (남는 예산 // 평균보다 큰 애들 수)를 해줘서 평균 예산에 더해서 답을 구한다.
-반례:
-4
-122 110 140 150
-485
-정답이 125가 나와야 하는데, 내 구성대로라면 밑을 그냥 버리기 때문에, 124가 나온다
+접근: 간단한 이진탐색으로 풀 수 있었다. 코드 구현을 못 했다.
 
 시간복잡도:
 '''
 
 N = int(input())
 
-required = sorted(list(map(int, input().split())))
+nums = list(map(int, input().split()))
 
 budget = int(input())
 
-limit = budget // N
-
-over_cnt = 0
-remaining = 0
-
-for i in range(N):
-    if required[i] > limit:
-        over_cnt += 1
-    else:
-        remaining += limit - required[i]
+lower = budget // N
+upper = max(nums)
 
 
-if sum(required) < budget:
-    print(required[-1])
+if sum(nums) <= budget:
+  print(max(nums))
+
 else:
-    print(limit + remaining//over_cnt)
+  while lower <= upper:
+    under, over = 0, 0
+    mid = (lower + upper) // 2
+
+    for i in range(N):
+      if nums[i] <= mid:
+        under += nums[i]
+      else:
+        over += mid
+
+    if under + over <= budget:
+      lower = mid + 1
+    elif under + over > budget:
+      upper = mid - 1
+
+
+
+  print(lower-1)
+  # print(upper-1)
+  # print(mid)
